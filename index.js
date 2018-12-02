0e0ce79356254e67747c270b1199613d6e744d1f
BaRoNeaS <41582600+BaRoNeaS@users.noreply.github.com>
2018-12-02 20:15:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,88 +1,35 @@
-const {FriendlyError, SQLiteProvider} = require('discord.js-commando');
-const {MessageEmbed} = require('discord.js');
-const moment = require('moment');
-const path = require('path');
-const sqlite = require('sqlite');
-const config = require('./data/config.json');
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-const PonyCommandoClient = require('./lib/PonyCommandoClient');
-const PonyUtils = require('./lib/PonyUtils');
+const newUsers = [];
 
-const client = new PonyCommandoClient({
-    commandPrefix: config.PREFIX,
-    owner: config.ADMIN,
-    disableEveryone: true,
-    unknownCommandResponse: false,
-    disabledEvents: ["TYPING_START"]
+client.on("ready", () => {
+  console.log("I am ready!");
 });
 
-sqlite.open(path.join(__dirname, "/data/settings.sqlite3")).then((db) => {
-    client.setProvider(new SQLiteProvider(db));
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
 });
 
-client.registry
-    .registerDefaultTypes()
-    .registerGroups([
-        ['general', 'General'],
-        ['levels', 'Levels'],
-        ['admin', 'Admin']
-    ])
-    .registerCommandsIn(path.join(__dirname, 'commands'));
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
 
-client.on('ready', async () => {
-    client.logger.info('Logged in!');
-    await client.user.setActivity(`Level Pony`);
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.find(channel => channel.name === "general").send("Welcome our new users!\n" + userlist);
+    newUsers[guild.id].clear();
+  }
 });
 
-
-client.on('guildCreate', async (guild) => {
-    if (!guild.available) return;
-
-    const embed = new MessageEmbed({
-        author: {
-            name: "Hello, I'm Level Pony!",
-            iconURL: client.user.displayAvatarURL()
-        },
-        description: `You've just added me to **${guild.name}**.\n\nHere is some information about myself:\n\n${await PonyUtils.getHelpText(guild)}`,
-
-        timestamp: moment().format('LLL'),
-        footer: {
-            text: client.user.tag
-        }
-    });
-
-    guild.owner.send({embed});
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
 });
 
-client.on('message', async (message) => {
-    if (message.author.id === client.user.id) {
-        return;
-    }
-
-    if (message.channel.type === 'group') {
-        return;
-    }
-
-    if (message.author.bot) {
-        return;
-    }
-
-    if(!message.guild){
-        return
-    }
-
-    await client.levels.giveGuildUserExp(message.guild.members.get(message.author.id), message);
-
-});
-
-client.on('commandError', (cmd, err) => {
-    if (err instanceof FriendlyError) return;
-    client.logger.error(`Error in command ${cmd.groupID }: ${cmd.memberName} ${err}`);
-});
-
-client.on('commandBlocked', (msg, reason) => {
-    client.logger.warn(`Command [${msg.command.groupID}:${msg.command.memberName}] blocked. Reason: ${reason}`);
-});
 
 client.login(config.TOKEN).catch((err) => {
     client.logger.error(err);
